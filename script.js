2bf434527b41ce98428141b477410fe90074ca26
witherpearl <83099794+witherpearl@users.noreply.github.com>
2022-03-26 19:19:08 -0500

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementsByTagName("Name")[0].style.fontSize = "2vw";
-document.getElementsByTagName("Name")[0].style.color = "#A020F0"
-document.getElementsByTagName("Games")[0].style.fontSize = "20px";
-document.getElementsByTagName("Games")[0].style.color = "#A020F0"
